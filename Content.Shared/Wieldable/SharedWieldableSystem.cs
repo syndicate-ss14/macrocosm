189774b40db2913fe9fd6b26091c7dae8551e07a
Kip <32859367+kipdotnet@users.noreply.github.com>
2026-03-15 12:49:34 +1100

    one handed wielding
    Co-Authored-By: mq <113324899+mqole@users.noreply.github.com>

--- Content.Shared/Wieldable/SharedWieldableSystem.cs
+++ Content.Shared/Wieldable/SharedWieldableSystem.cs
@@ -24,6 +24,7 @@ using Content.Shared.Wieldable.Components;
 using Robust.Shared.Audio.Systems;
 using Robust.Shared.Collections;
 using Robust.Shared.Timing;
+using Content.Shared._MACRO.Decapoids; // Macrocosm edit
 
 namespace Content.Shared.Wieldable;
 
@@ -259,7 +260,8 @@ public abstract class SharedWieldableSystem : EntitySystem
             return false;
         }
 
-        if (_hands.CountFreeableHands((user, hands), except: uid) < component.FreeHandsRequired)
+        if (_hands.CountFreeableHands((user, hands), except: uid) < component.FreeHandsRequired
+            && !HasComp<CanWieldOneHandedComponent>(user)) // Macrocosm add
         {
             if (!quiet)
             {
