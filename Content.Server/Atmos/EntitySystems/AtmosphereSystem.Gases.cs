7af2e2488056b87f59b05e4c8e77580ee4c36018
CombustibleToast <34487106+CombustibleToast@users.noreply.github.com>
2026-04-29 07:27:19 -0400

    change specificheat to molarheatcapacity

--- Content.Server/Atmos/EntitySystems/AtmosphereSystem.Gases.cs
+++ Content.Server/Atmos/EntitySystems/AtmosphereSystem.Gases.cs
@@ -66,7 +66,7 @@ namespace Content.Server.Atmos.EntitySystems
 
             //Multiply fractional moles with specific heats and return the sum. This is the specific heat of the mixture.
             //Here temp contains the partial specific heat of each gas in the mixture.
-            NumericsHelpers.Multiply(temp, GasSpecificHeats, temp);
+            NumericsHelpers.Multiply(temp, GasMolarHeatCapacities, temp);
             return NumericsHelpers.HorizontalAdd(temp);
         }
         // END MACRO ADD
